518ee0c08362d00941365816eda53be92f447e13
Your Name <you@example.com>
2017-10-25 16:26:33 -0300

    'Formulario'

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace nuevo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mi nombre es:");
+            Console.ReadLine();
+            Console.WriteLine("Edad:");
+            Console.ReadLine();
+            Console.WriteLine ("Color favorito");
+            Console.ReadLine();
+            Console.ReadKey();
+            
+            
+           
+        }
+    }
+}
